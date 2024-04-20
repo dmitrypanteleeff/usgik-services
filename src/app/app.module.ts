c83c0e73c0e7f5bcc3a67bb68de4c782aa4d808b
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-04-21 00:33:51 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
+import { NgLetModule } from 'ng-let';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    NgLetModule,
+    LeafletModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
